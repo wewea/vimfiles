e734cccbdf74c4d2f14c8f1e375f261a606a9bf4
vimquee <vimquee@live.com>
2015-08-25 19:58:43 +0800

    add script

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir vimfiles
 UPLOAD_FILE="$HOME/.vim/dict"
 UPLOAD_FILE="$UPLOAD_FILE $HOME/.vim/colors"
 UPLOAD_FILE="$UPLOAD_FILE $HOME/.vimrc"
@@ -7,5 +8,12 @@ UPLOAD_FILE="$UPLOAD_FILE $HOME/.zshrc"
 
 echo $UPLOAD_FILE
 
-cp -r $UPLOAD_FILE ./
+if [ -d "vimfiles" ]; then
+	cp -r $UPLOAD_FILE ./vimfiles
+else
+	echo "lack of dir(vimfiles)"
+	exit 1
+fi
+
+
 
